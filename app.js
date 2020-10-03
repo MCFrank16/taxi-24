dd0b65d89083f97bc8226d96b4a548cb88eb16ed
MCFrank16 <boonatechnologies@gmail.com>
2020-10-03 15:47:09 +0200

    setting up the project

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const log = require('./source/helper/logger');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'success', message: 'Fleet Management System' });
+});
+
+app.listen(port, () => {
+  log.info(`🚀 Application running on: http://localhost:${port}`);
+});
+
+module.exports = app;
